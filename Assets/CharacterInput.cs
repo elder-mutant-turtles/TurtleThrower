d5cd4c83365912c786b57c1bd748e8caf379dc17
Jun Fujioka <felipe.fujioka@tapps.com.br>
2019-01-27 08:44:44 -0200

    keyboard input

--- Assets/CharacterInput.cs
+++ Assets/CharacterInput.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterInput : MonoBehaviour
 {
 
+	private const string BUTTON_1 = "j1a1";
+	private const string BUTTON_0 = "j1a0";
+	private const string BUTTON_5 = "j1a5";
+	private const string KEY_UP = "up";
+	private const string KEY_RIGHT = "right";
+	private const string KEY_LEFT = "left";
+	private const string KEY_Z = "z";
+	private const string KEY_SPACE = "space";
+	private const string AXIS_H = "j1axis1";
+	
 	public CharacterController2D controller;
 
 	private bool jump; 
@@ -12,26 +23,26 @@ public class CharacterInput : MonoBehaviour
 	// Update is called once per frame
 	private void Update()
 	{
-		if (Input.GetButtonDown("j1a0"))
+		if (JumpInputPressed())
 		{
 			jump = true;
 		}
 
-		if (Input.GetButtonDown("j1a1"))
+		if (ActionInputPressed())
 		{
 			controller.Interact();
 		}
 
-		if (controller.ShellIsEquipped() && Input.GetButtonDown("j1a1"))
+		if (controller.ShellIsEquipped() && AimInputPressed())
 		{
 			controller.Lift(true);
 		} 
-		else if (controller.IsLifting() && Input.GetButtonUp("j1a1"))
+		else if (controller.IsLifting() && AimInputUp())
 		{
 			controller.Lift(false);
 		}
 
-		if (controller.IsLifting() && Input.GetButtonDown("j1a5"))
+		if (controller.IsLifting() && ActionInputPressed())
 		{
 			controller.Throw();
 		}
@@ -39,7 +50,7 @@ public class CharacterInput : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		var hAxis = Input.GetAxis("j1axis1");
+		var hAxis = HorizontalAxisMove();
 
 		controller.Move(hAxis);	
 		
@@ -48,5 +59,38 @@ public class CharacterInput : MonoBehaviour
 			controller.Jump();
 			jump = false;
 		}
+	}
+
+	private bool JumpInputPressed()
+	{
+		return Input.GetButtonDown(BUTTON_1) || Input.GetKeyDown(KEY_SPACE);
+	}
+
+	private bool ActionInputPressed()
+	{
+		return Input.GetButtonDown(BUTTON_0) || Input.GetKeyDown(KEY_Z);
+	}
+
+	private bool AimInputPressed()
+	{
+		return Input.GetButtonDown(BUTTON_5) || Input.GetKeyDown(KEY_UP);
+	}
+
+	private bool AimInputUp()
+	{
+		return Input.GetButtonUp(BUTTON_5) || Input.GetKeyUp(KEY_UP);
+	}
+
+	private float HorizontalAxisMove()
+	{
+		var jAxis = Input.GetAxis(AXIS_H);
+
+		var kAxis = Input.GetKey(KEY_RIGHT) ? 1 : 0;
+
+		kAxis += Input.GetKey(KEY_LEFT) ? -1 : 0;
+
+		var value = kAxis + jAxis;
+
+		return Mathf.Clamp(value, -1f, 1f);
 	}
 }
