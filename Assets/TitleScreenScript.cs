f0d044222da5ca9aae39ee7e2df93c75fdeb671f
Jun Fujioka <felipe.fujioka@tapps.com.br>
2019-01-27 02:38:51 -0200

    title screen loads final level

--- Assets/TitleScreenScript.cs
+++ Assets/TitleScreenScript.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("j1a1"))
+		{
+			SceneManager.LoadScene("FinalLevel");
+		}		
 	}
 }
