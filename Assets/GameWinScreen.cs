b0ee6e6e780e87fbb5451fde9146e61b712cda53
Jun Fujioka <felipe.fujioka@tapps.com.br>
2019-01-27 16:56:39 -0200

    fix win screen f command

--- Assets/GameWinScreen.cs
+++ Assets/GameWinScreen.cs
@@ -10,6 +10,8 @@ public class GameWinScreen : MonoBehaviour
 
     public static GameWinScreen Instance;
 
+    private bool shown = false;
+
     private void Awake()
     {
         Instance = this;
@@ -20,19 +22,23 @@ public class GameWinScreen : MonoBehaviour
         CanvasAlpha.DOFade(1, 0.3f).SetUpdate(true);
 
         Time.timeScale = 0f;
+
+        shown = true;
     }
 
     public void Hide()
     {
         CanvasAlpha.DOFade(0f, 0.3f).SetUpdate(true);
         Application.Quit();
+
+        shown = false;
         
         Application.OpenURL("https://www.washingtonpost.com/news/animalia/wp/2016/09/19/with-800-offspring-very-sexually-active-tortoise-saves-species-from-extinction/?noredirect=on&utm_term=.20b64c7a9d5c");
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown("f"))
+        if (shown && Input.GetKeyDown("f"))
         {
             Hide();
         }
