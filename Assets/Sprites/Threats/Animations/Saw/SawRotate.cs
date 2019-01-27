91363da1a7bdd4414cd17d0b88c1fa4577888778
Carlos Lacombe <carlos.lacombe@tapps.com.br>
2019-01-26 22:40:53 -0200

    added deadly saw

---
+++ Assets/Sprites/Threats/Animations/Saw/SawRotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurtleThrower
+{
+
+
+    public class SawRotate : MonoBehaviour
+    {
+
+        public Transform sawObject;
+
+        public float RotateSpeed = 1.0f;
+        
+        // Use this for initialization
+        void Start () 
+        {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () 
+        {
+		    sawObject.Rotate(Vector3.forward, RotateSpeed * Time.deltaTime);
+        }
+        
+        
+    }
+
+}
